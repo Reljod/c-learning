f1ffdddf7554daf4a2df0d6e43be6ff5e90328e3
Reljod Oreta <oretareljod@gmail.com>
2023-08-28 15:47:26 +0800

    create a program that uses scanf

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    printf("Start a program\n\n");
+
+    int age;
+
+    printf("Tell me your age: ");
+    scanf("%d", &age);
+    printf("You entered %d", age);
+
+    getchar();
+
     return 0;
 }
