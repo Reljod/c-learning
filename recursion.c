aeb24f6734f373b96456e82fdf8c33e6f162e0fb
Reljod Oreta <oretareljod@gmail.com>
2023-08-28 22:59:24 +0800

    apply function pointers to recursion to inject printing and any other function to compute next number

--- recursion.c
+++ recursion.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+typedef void (*apply_curr_num)(int);
+
 void run_fibonnaci(int, int, int);
-int compute_next_number(int, int, int);
+int compute_next_number(int, int, int, apply_curr_num);
+void print_number(int);
 
 int main() {
     int prev = 0;
@@ -14,13 +17,18 @@ int main() {
 }
 
 void run_fibonnaci(int prev, int curr, int u_limit){
-    compute_next_number(prev, curr, u_limit);
+    apply_curr_num func = &print_number;
+    compute_next_number(prev, curr, u_limit, func);
 }
 
-int compute_next_number(int prev, int curr, int u_limit) {
-    printf("%d\n", curr);
+void print_number(int num) {
+    printf("%d\n", num);
+}
+
+int compute_next_number(int prev, int curr, int u_limit, apply_curr_num func) {
+    func(curr);
     if (prev + curr < u_limit) {
-        return compute_next_number(curr, prev + curr, u_limit);
+        return compute_next_number(curr, prev + curr, u_limit, func);
     } else {
         return curr;
     }
