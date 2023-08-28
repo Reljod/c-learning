cc43f1df6986f379c050b698f99487938107b232
Reljod Oreta <oretareljod@gmail.com>
2023-08-28 15:56:18 +0800

    expand the code to include variable assignment derived from another variable

--- main.c
+++ main.c
@@ -5,7 +5,21 @@ int main() {
     float gpa = 1.75;
     char grade = 'A';
 
-    printf("A student scores %d in an exam.\nAn okay score for someone with gpa of %f and grade %c\n", score, gpa, grade);
+    printf(
+        "A student scores %d in an exam.\nAn okay score for someone with gpa of %f and grade %c\n",
+        score, gpa, grade
+    );
+
+    int score1, score2, score3;
+
+    score1 = score + 1;
+    score2 = score - 1;
+    score3 = score - 2;
+
+    printf(
+        "A three different scores, (%d, %d, %d) were derived from the first score: %d\n",
+        score1, score2, score3, score
+    );
 
     return 0;
 }
