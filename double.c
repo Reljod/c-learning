675f0c0b41e722eb65f3fa4888c98925c8c85118
Reljod Oreta <oretareljod@gmail.com>
2023-08-28 16:09:12 +0800

    create a program that calculates circumference to use double and constant lessons

---
+++ double.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double get_circumference(float radius, double pi) {
+    return pi * radius * radius;
+}
+
+int main() {
+    const double PI = 3.1415926536;
+
+    printf("The current value of pi is %.10lf\n", PI);
+
+
+    float radius = 12.0;
+    double circum_val = get_circumference(radius, PI);
+
+    printf(
+        "Given pi is %.10lf and radius of %.4f, the circumference of circle is %.10lf\n",
+        PI, radius, circum_val
+    );
+
+    return 0;
+}
